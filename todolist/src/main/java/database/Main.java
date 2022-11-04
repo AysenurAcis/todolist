a13f35b3acc7250e7c1f1d686e9b31e76910df12
AysenurAcis <101017688+AysenurAcis@users.noreply.github.com>
2022-11-04 17:03:17 +0300

    Add files via upload

---
+++ todolist/src/main/java/database/Main.java
@@ -0,0 +1,44 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String connectionUrl =
+            "jdbc:sqlserver://localhost\\MSSQLSERVER01;"
+                + "database=todo;"
+                + "user=\\aysen;"
+                + "password=;"
+                + "encrypt=true;"
+                + "trustServerCertificate=false;"
+                + "loginTimeout=30;";
+
+        String insertSql = "INSERT INTO SalesLT.Product (Name, ProductNumber, Color, StandardCost, ListPrice, SellStartDate) VALUES "
+                + "('NewBike', 'BikeNew', 'Blue', 50, 120, '2016-01-01');";
+
+        ResultSet resultSet = null;
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+             PreparedStatement prepsInsertProduct = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);) {
+
+            prepsInsertProduct.execute();
+            // Retrieve the generated key from the insert.
+            resultSet = prepsInsertProduct.getGeneratedKeys();
+
+            // Print the ID of the inserted row.
+            while (resultSet.next()) {
+                System.out.println("Generated: " + resultSet.getString(1));
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
